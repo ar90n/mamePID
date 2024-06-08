252c2cf49fb6eb70072163ca3a3c9ca0a1cea009
Masahiro Wada <argon.argon.argon@gmail.com>
2024-06-08 07:39:30 +0000

    intiial implementation

---
+++ src/mamePID.hpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+namespace mamePID
+{
+    template <typename T>
+    class PID
+    {
+    public:
+        PID(T kp, T ki, T kd, T dt, T min, T max)
+            : kp(kp), ki(ki), kd(kd), dt(dt), min(min), max(max), pre_error(0),
+              integral(0)
+        {
+        }
+
+        T calculate(T setpoint, T pv)
+        {
+            T error = setpoint - pv;
+            T derivative = (error - pre_error) / dt;
+
+            T output = kp * error + ki * integral + kd * derivative;
+            if (output > max)
+                output = max;
+            else if (output < min)
+                output = min;
+
+            integral += error * dt;
+            pre_error = error;
+            return output;
+        }
+
+    private:
+        T kp, ki, kd, dt, min, max;
+        T pre_error, integral;
+    };
+} // namespace mamePID
